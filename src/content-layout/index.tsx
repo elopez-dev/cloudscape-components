839d444128b0bf2d86327c9d16de85b20699f130
Scott O'Brien <scottob@amazon.com>
2022-12-19 16:10:56 -0500

    Proof of concept sticky header behavior.

--- src/content-layout/index.tsx
+++ src/content-layout/index.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import clsx from 'clsx';
 import { AppLayoutContext } from '../app-layout/visual-refresh/context';
 import { applyDisplayName } from '../internal/utils/apply-display-name';
@@ -16,7 +16,7 @@ export { ContentLayoutProps };
 
 export default function ContentLayout({ children, disableOverlap, header, ...rest }: ContentLayoutProps) {
   const baseProps = getBaseProps(rest);
-  const { breadcrumbs } = useContext(AppLayoutContext);
+  const { breadcrumbs, setHasStickyBackground } = useContext(AppLayoutContext);
 
   const rootElement = useRef<HTMLDivElement>(null);
   const { __internalRootRef } = useBaseComponent('ContentLayout');
@@ -25,11 +25,25 @@ export default function ContentLayout({ children, disableOverlap, header, ...res
   const isVisualRefresh = useVisualRefresh();
 
   /**
+   * Handle sticky header integration with the AppLayout.
+   */
+  const hasStickyHeader = true;
+
+  useEffect(
+    function handleHasStickyBackground() {
+      if (hasStickyHeader && isVisualRefresh) {
+        setHasStickyBackground(true);
+      }
+    },
+    [hasStickyHeader, isVisualRefresh, setHasStickyBackground]
+  );
+
+  /**
    * Disable the overlap if the component is missing either a header or child
    * content. If the component is not using visual refresh then the overlap
    * will not be displayed at all. This is handled in the CSS not the JavaScript.
    */
-  const isOverlapDisabled = !children || !header || disableOverlap;
+  const isOverlapDisabled = !children || !header || disableOverlap || hasStickyHeader;
 
   return (
     <div
@@ -37,6 +51,7 @@ export default function ContentLayout({ children, disableOverlap, header, ...res
       className={clsx(baseProps.className, styles.layout, {
         [styles['is-overlap-disabled']]: isOverlapDisabled,
         [styles['is-visual-refresh']]: isVisualRefresh,
+        [styles['has-sticky-header']]: hasStickyHeader,
       })}
       ref={mergedRef}
     >
